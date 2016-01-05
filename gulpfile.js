867efad920963c777b6d1eb857b5b4901c6bae24
NPoling <npoling@gmail.com>
2016-01-05 15:57:09 -0600

    Fixed bug in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('app/*.scss', ['sass']);
 });
 
 // Default Task
