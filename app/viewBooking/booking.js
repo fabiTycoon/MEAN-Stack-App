2f68830fb9199eca66e0cd525c3fce6adc80b8e0
NPoling <npoling@gmail.com>
2016-01-06 11:15:00 -0600

    Framed out routes for reservation system

--- app/viewBooking/booking.js
+++ app/viewBooking/booking.js
@@ -3,14 +3,42 @@
 angular.module('myApp.bookingView', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/booking', {
+  $routeProvider.when('/book', {
     templateUrl: 'viewBooking/booking.html',
     controller: 'BookingViewCtrl'
   });
+  $routeProvider.when('/bookDog', {
+    templateUrl: 'viewBooking/bookDog.html',
+    controller: 'BookingViewCtrl'
+  });
+  $routeProvider.when('/bookCat', {
+    templateUrl: 'viewBooking/bookCat.html',
+    controller: 'BookingViewCtrl'
+  });
+  $routeProvider.when('/bookNext', {
+    templateUrl: 'viewBooking/bookNext.html',
+    controller: 'BookingViewCtrl'
+  });
+
+
+
 }])
 
 .controller('BookingViewCtrl', [function() {
   
+  $scope.currentPets = [];
+  $scope.owner = {
+    first: '',
+    last: '',
+    phone: '',
+    email: '',
+    dates: []
+  }
+  $scope.currentPet = {
+    species: '',
+    name: ''
+
+  }
 
 
 }]);
