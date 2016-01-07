83576753c78276c2ec1b084d39d11fb1a4751acb
NPoling <npoling@gmail.com>
2016-01-07 15:22:29 -0600

    Refactored view logic for about page and changed size of phone input boxes on booking page.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,40 +11,20 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
 
-  $scope.facilSel = true;
-  $scope.teamSel = false;
-  $scope.faqSel = false;
-  $scope.contactSel = false;
+  $scope.select = {
+    facil: true,
+    team: false,
+    faq: false,
+    contact: false
+  }
 
-  $scope.selFacil = function () {
-    console.log("selFacil called")
-    $scope.facilSel = true;
-    $scope.teamSel = false;
-    $scope.faqSel = false;
-    $scope.contactSel = false;
+  $scope.selector = function (selectorKey) {
+    for (var key in $scope.select) {
+      $scope.select[key] = false;
+    }
+    $scope.select[selectorKey] = true;
+    console.log($scope.select);
   };
 
-  $scope.selTeam = function () {
-    console.log("selTeam called")
-    $scope.facilSel = false;
-    $scope.teamSel = true;
-    $scope.faqSel = false;
-    $scope.contactSel = false;
-  };
 
-  $scope.selFaq = function () {
-    console.log("selFaq called")
-    $scope.facilSel = false;
-    $scope.teamSel = false;
-    $scope.faqSel = true;
-    $scope.contactSel = false;
-  };
-
-  $scope.selContact = function () {
-    console.log("selContact called")
-    $scope.facilSel = false;
-    $scope.teamSel = false;
-    $scope.faqSel = false;
-    $scope.contactSel = true;
-  };
 }]);
