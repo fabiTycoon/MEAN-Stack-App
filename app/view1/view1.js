20f48c88ad85a9d51ac36ee0cd97b634a138f34a
NPoling <npoling@gmail.com>
2016-01-06 18:18:04 -0600

    View logic for about page.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,7 +9,42 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['scope', function($scope) {
-  
-  $scope.menuSelect = 'facil';
+.controller('View1Ctrl', ['$scope', function($scope) {
+
+  $scope.facilSel = true;
+  $scope.teamSel = false;
+  $scope.faqSel = false;
+  $scope.contactSel = false;
+
+  $scope.selFacil = function () {
+    console.log("selFacil called")
+    $scope.facilSel = true;
+    $scope.teamSel = false;
+    $scope.faqSel = false;
+    $scope.contactSel = false;
+  };
+
+  $scope.selTeam = function () {
+    console.log("selTeam called")
+    $scope.facilSel = false;
+    $scope.teamSel = true;
+    $scope.faqSel = false;
+    $scope.contactSel = false;
+  };
+
+  $scope.selFaq = function () {
+    console.log("selFaq called")
+    $scope.facilSel = false;
+    $scope.teamSel = false;
+    $scope.faqSel = true;
+    $scope.contactSel = false;
+  };
+
+  $scope.selContact = function () {
+    console.log("selContact called")
+    $scope.facilSel = false;
+    $scope.teamSel = false;
+    $scope.faqSel = false;
+    $scope.contactSel = true;
+  };
 }]);
