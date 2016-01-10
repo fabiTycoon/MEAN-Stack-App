3330c3b07765094d1f115805dac8183bf5f2a592
NPoling <npoling@gmail.com>
2016-01-10 12:38:58 -0600

    Fixed syntax error with controller scope injection

--- app/viewBooking/addPet.js
+++ app/viewBooking/addPet.js
@@ -9,7 +9,39 @@ angular.module('myApp.addPetView', ['ngRoute'])
   });
 }])
 
-.controller('AddPetCtrl', [function() {
+.controller('AddPetCtrl', [ '$scope', function($scope) {
+
+  $scope.currentPets = [];
+
+  $scope.currentPet = {
+    species: '',
+    name: ''
+  };
+
+  //Controls Add a Pet View display
+  $scope.dogSelected = false;
+  $scope.catSelected = false;
+  $scope.otherSelected = false;
+
+  $scope.speciesSelector = function (species) {
+    console.log("speciesSel called, species val is", species);
+
+    if (species === 'dog') {
+      $scope.currentPet[species] = 'dog';
+      $scope.dogSelected = true;
+      $scope.catSelected = false;
+      $scope.otherSelected = false;
+    } else if (species === 'cat') {
+      $scope.currentPet[species] = 'cat';
+      $scope.dogSelected = false;
+      $scope.catSelected = true;
+      $scope.otherSelected = false;
+    } else if (species === 'other') {
+      $scope.dogSelected = false;
+      $scope.catSelected = false;
+      $scope.otherSelected = true;
+    }
+  };
 
 }])
 
