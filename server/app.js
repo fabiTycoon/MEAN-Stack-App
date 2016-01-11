e55f70440aeb537f39300732cecd893068044cef
NPoling <npoling@gmail.com>
2016-01-11 14:44:25 -0600

    Further work on setting up Express routes

--- server/app.js
+++ server/app.js
@@ -2,9 +2,29 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var db = require('./config/db');
-
 var port = process.env.PORT || 8080; 
+var assetFolder = Path.resolve(__dirname, '../app/');
 
 // parse application/json 
 app.use(bodyParser.json());
+
+
+//Define API endpoints for users, pets, and reservations:
+
+//User:
+app.get('/user', function (req, res) {
+  res.send()
+});
+
+app.post('/user', function (req, res) {
+  //create a new user
+  res.send()
+});
+
+
+
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(assetFolder + '/index.html');
+});
