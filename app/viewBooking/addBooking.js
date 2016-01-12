125ff53f35b6e1c18bc8a87595f3db34451845c6
NPoling <npoling@gmail.com>
2016-01-12 17:52:08 -0600

    Fixed initialization of datepicker in booking view

--- app/viewBooking/addBooking.js
+++ app/viewBooking/addBooking.js
@@ -20,7 +20,14 @@ angular.module('myApp.viewAddBooking', ['ngRoute'])
     checkOutTime: ''
   }
 
+  var init = function () {
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year
+    });
+  }
 
+  init();
 }]);
 
 ;
--- app/viewBooking/addUser.js
+++ app/viewBooking/addUser.js
@@ -26,10 +26,6 @@ angular.module('myApp.viewAddUser', ['ngRoute'])
 
   var init = function () {
     $('select').material_select();    
-    $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15, // Creates a dropdown of 15 years to control year
-    });
   };
 
   init();
