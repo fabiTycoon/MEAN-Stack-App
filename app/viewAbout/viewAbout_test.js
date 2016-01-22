cbfaea6a7ef57047b6a3fc66fbe5a5de04dbdcdf
NPoling <npoling@gmail.com>
2016-01-22 11:32:19 -0600

    First go at unit tests for about page.

--- app/viewAbout/viewAbout_test.js
+++ app/viewAbout/viewAbout_test.js
@@ -1,15 +1,47 @@
 'use strict';
 
 describe('myApp.viewAbout module', function() {
-
+  //using module fn provided by angular mocks; loads given module for testing
   beforeEach(module('myApp.viewAbout'));
+
+  beforeEach(inject(function(_$controller_){
+    // The injector unwraps the underscores (_) from around the parameter names when matching
+    $controller = _$controller_;
+  }));
 
   describe('viewAbout controller', function(){
 
-    it('should ....', inject(function($controller) {
+    var $controller;
+
+    it('should exist', inject(function($controller) {
       var ViewAboutCtrl = $controller('ViewAboutCtrl');
       expect(ViewAboutCtrl).toBeDefined();
     }));
 
   });
+
+  describe('$scope.selector', function(){
+    var $scope, controller;
+
+    beforeEach(function() {
+      $scope = {};
+      controller = $controller('ViewAboutCtrl', { $scope: $scope });
+    });
+
+      it('should select the correct item', function($scope){
+        $scope.selector('team');
+        expect($scope.select.team).toEqual(true); 
+      });
+
+      it('should allow only one item to be selected', function(){
+        var trueCount = 0;
+        for (var selectorKey in $scope.select) {
+          if ($scope.select[selectorKey] === true) {
+            trueCount++;
+          }
+        }
+        expect(trueCount).toEqual(1);
+      });
+  });
+
 });
