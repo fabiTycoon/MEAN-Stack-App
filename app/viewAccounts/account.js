f3dd37f21e3df659d7a96ef9805882888e9de0d3
NPoling <npoling@gmail.com>
2016-08-08 20:16:57 -0500

    Added additional button to main page view.  Added tabbed user profile view

--- app/viewAccounts/account.js
+++ app/viewAccounts/account.js
@@ -11,40 +11,48 @@ angular.module('myApp.viewAccount', ['ngRoute'])
 
 .controller('viewAccountCtrl', [ '$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
 
+  $scope.displayName = '';
+
   $scope.accountViewModelState = {
     userInfo: true,
     petInfo: false,
     reservationInfo: false
   };
 
-  $scope.setDefaultState = function (reset) {
+  if ($rootScope.user) {
+    $scope.displayName = $rootScope.user.first + " " + rootScope.user.last;
+    console.log("SET DISPLAY NAME: ", $scope.displayName);
+  };
 
+  $scope.setDefaultState = function (reset) {
     for (var state in $scope.accountViewModelState) {
       $scope.accountViewModelState[state] = false;
     };
 
     if (arguments.length !== 0) {
-      $scope.accountViewModelState.petInfo = true;
+      $scope.accountViewModelState.userInfo = true;
     };
-
-
   };
+
+  $scope.setDefaultState(true);
+
 
   $scope.showUserInfo = function () {
-    $scope.setDefaultState();
     $scope.profileTitle = "MY ACCOUNT:";
-    $scope.accountViewModelState.userInfo = true;
+    $scope.setDefaultState(true);
   };
-  
+
+
   $scope.showProfilePets = function () {
     $scope.setDefaultState();
     $scope.profileTitle = "MY SAVED PETS:";
     $scope.accountViewModelState.petInfo = true;
   };
 
+
   $scope.showProfileReservations = function () {
     $scope.setDefaultState();
-    $scope.profileTitle = "MY RESERVATIONS";
+    $scope.profileTitle = "MY RESERVATIONS:";
     $scope.accountViewModelState.reservationInfo = true;
   };
 
